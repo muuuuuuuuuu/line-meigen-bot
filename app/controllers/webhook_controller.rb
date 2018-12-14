97b364d1aa4a50561c29af57244c2cdce55cde00
katuya nakamura <kakatutuyaya.0106@icloud.com>
2018-12-14 11:25:28 +0900

    webhookcontrollerにrequireを追加

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,15 +1,16 @@
 class WebhookController < ApplicationController
-
+  require 'line/bot'
+  
 def meigendayo
   message = {
             type: 'text',
             text: '名言'
-  }
+           }
             client = Line::Bot::Client.new { |config|
               config.channel_secret = "<channel secret>"
               config.channel_token = "<channel access token>"
-  }
-    response = client.push_message("<to>", message)
-      p response
+           }
+  response = client.push_message("<to>", message)
+    p response
   end
 end
--- lib/tasks/reminder_task.rake
+++ lib/tasks/reminder_task.rake
@@ -1,13 +1,5 @@
 namespace :reminder_task do
 
-  # desc "1日1回の名言"
-  # task :meigen do
-  #   a = ["努力", "勉強", "継続"]
-  #   puts a
-  #   努力
-  #   勉強
-  #   継続
-  # end
   desc "1日1回の名言"
   task :meigen => :environment do
     webhook = WebhookController.new
