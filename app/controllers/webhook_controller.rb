0f2819722ef84802078ce22279c09937c9851660
katuya nakamura <kakatutuyaya.0106@icloud.com>
2018-12-14 12:27:41 +0900

    channelselect token to quition

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,15 +1,18 @@
 class WebhookController < ApplicationController
   require 'line/bot'
-  
+
 def meigendayo
   message = {
             type: 'text',
             text: '名言'
            }
             client = Line::Bot::Client.new { |config|
+                                         # ↓ここと
               config.channel_secret = "<channel secret>"
+                                          # ↓ここ
               config.channel_token = "<channel access token>"
            }
+                                  # ↓ここがわかりません
   response = client.push_message("<to>", message)
     p response
   end
