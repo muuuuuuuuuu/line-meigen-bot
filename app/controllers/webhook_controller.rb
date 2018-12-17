4dcd1d850cff3771e7fbdf7ca09eb27a46b129a6
katuya nakamura <kakatutuyaya.0106@icloud.com>
2018-12-17 20:23:18 +0900

    webhookcotroller meigendayo message

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -7,11 +7,11 @@ def meigendayo
             "text": "名言"
            }
             client = Line::Bot::Client.new { |config|
-              config.channel_secret = "LINE_CHANNEL_SECRET"
-              config.channel_token = "LINE_CHANNEL_TOKEN"
+              config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+              config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
 
            }
-  response = client.push_message("LINE_CHANNEL_ID", message)
+  response = client.push_message(ENV["LINE_CHANNEL_ID"], message)
     p response
   end
 end
