8db3ff499f11bc59aa71b1e2d443bc2cec94cbf3
katuya nakamura <kakatutuyaya.0106@icloud.com>
2018-12-18 11:57:04 +0900

    buzfix

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -9,7 +9,6 @@ def meigendayo
             client = Line::Bot::Client.new { |config|
               config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
               config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-
            }
   response = client.push_message(ENV["LINE_USER_ID"], message)
 
