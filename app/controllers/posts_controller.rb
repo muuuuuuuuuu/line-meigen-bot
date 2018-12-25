d19d112a47556de1cee23c0a13502da9e4edac6f
katuya nakamura <kakatutuyaya.0106@icloud.com>
2018-12-25 15:41:11 +0900

    index.html.erbに個別にcssとjsを適用

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
 
   def index
+    @hello = '名言'
     render template: 'posts/index'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get '/posts' => 'posts#index'
+  get '/posts/new'
 
   get 'webhook/reminder'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root 'application#hello'
+  root 'application#hello'
   post '/callback' => 'linebot#callback'
 end
